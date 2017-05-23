4f815c923ba4895f058760c1f20853a892ca19db
arisgi <m1so.tama16@gmail.com>
2017-05-24 02:23:17 +0900

    Use ua-parser

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 import express from 'express';
+import uaParser from 'ua-parser-js';
 
 const app = express();
 
@@ -6,6 +7,12 @@ app.use(express.static('public'));
 
 app.get('/', (req, res) => {
   res.sendFile(`${__dirname}/public/index.html`);
+});
+
+app.get('/sp', (req, res) => {
+  const ua = uaParser(req.headers['user-agent']);
+
+  res.json(ua);
 });
 
 app.listen(3000);
