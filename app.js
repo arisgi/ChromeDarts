06cc1a5450e5d06d118407b6ea463ad4a974f950
arisgi <m1so.tama16@gmail.com>
2017-05-23 18:23:58 +0900

    Add static file

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ import express from 'express';
 const app = express();
 
 app.get('/', (req, res) => {
-  res.send('test');
+  res.sendFile(`${__dirname}/public/index.html`);
 });
 
 app.listen(3000);
