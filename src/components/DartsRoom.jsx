c35f694e02367795dda29e8a0c00b6f867f9c015
arisgi <m1so.tama16@gmail.com>
2017-05-25 02:17:06 +0900

    Display orientation

--- src/components/DartsRoom.jsx
+++ src/components/DartsRoom.jsx
@@ -6,15 +6,32 @@ export default class DartsRoom extends React.Component {
   constructor() {
     super();
     this.state = {
+      orientation: {
+        x: null,
+        y: null,
+        z: null,
+      },
       acceleration: {
         x: null,
         y: null,
         z: null,
       },
     };
+    this.handleDeviceOrientation = this.handleDeviceOrientation.bind(this);
     this.handleDeviceMotion = this.handleDeviceMotion.bind(this);
 
+    window.addEventListener('deviceorientation', this.handleDeviceOrientation);
     window.addEventListener('devicemotion', this.handleDeviceMotion);
+  }
+
+  handleDeviceOrientation(event) {
+    this.setState({
+      orientation: {
+        x: event.beta,
+        y: event.gamma,
+        z: event.alpha,
+      },
+    });
   }
 
   handleDeviceMotion(event) {
@@ -30,6 +47,9 @@ export default class DartsRoom extends React.Component {
   render() {
     return (
       <div>
+        {this.state.orientation.x && <p>orientation x : {this.state.orientation.x}</p>}
+        {this.state.orientation.y && <p>orientation y : {this.state.orientation.y}</p>}
+        {this.state.orientation.z && <p>orientation z : {this.state.orientation.z}</p>}
         {this.state.acceleration.x && <p>acceleration x : {this.state.acceleration.x}</p>}
         {this.state.acceleration.y && <p>acceleration y : {this.state.acceleration.y}</p>}
         {this.state.acceleration.z && <p>acceleration z : {this.state.acceleration.z}</p>}
