d148354bf1dd4d0af608af7094218fcbce60f3b6
arisgi <m1so.tama16@gmail.com>
2017-05-24 19:51:18 +0900

    Add form for login

--- src/components/LoginForm.jsx
+++ src/components/LoginForm.jsx
@@ -3,13 +3,30 @@ import React from 'react';
 export default class LoginForm extends React.Component {
   constructor() {
     super();
+    this.state = {
+      value: '',
+    };
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+
+  handleChange(event) {
+    this.setState({
+      value: event.target.value,
+    });
+  }
+
+  handleSubmit(event) {
+    this.props.handleLogin(event.target.children[0].value);
+    event.preventDefault();
   }
 
   render() {
     return (
-      <div>
-        LoginForm
-      </div>
+      <form onSubmit={this.handleSubmit}>
+        <input type="text" value={this.state.value} onChange={this.handleChange} />
+        <input type="submit" value="Submit" />
+      </form>
     );
   }
 }
--- src/components/Sp.jsx
+++ src/components/Sp.jsx
@@ -6,13 +6,22 @@ export default class Sp extends React.Component {
     super();
     this.state = {
       login: false,
+      name: '名無し',
     };
+    this.handleLogin = this.handleLogin.bind(this);
+  }
+
+  handleLogin(name) {
+    this.setState({
+      login: true,
+      name,
+    });
   }
 
   render() {
     return (
       <div>
-        {this.state.login === false && <LoginForm />}
+        {this.state.login === false && <LoginForm handleLogin={this.handleLogin} />}
       </div>
     );
   }
