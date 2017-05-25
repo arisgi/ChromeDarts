f4608ed9989215165f82bf4596b64608fff66474
arisgi <m1so.tama16@gmail.com>
2017-05-25 23:41:33 +0900

    Add routing

--- koa.js
+++ koa.js
@@ -1,9 +1,18 @@
 import Koa from 'koa';
+import Router from 'koa-router';
 
 const app = new Koa();
+const router = Router();
 
-app.use((ctx) => {
-  ctx.body = 'Koa test';
+router.get('/', (ctx, next) => {
+  ctx.body = '/';
 });
+
+router.get('/sp', (ctx, next) => {
+  ctx.body = '/sp';
+});
+
+app.use(router.routes());
+app.use(router.allowedMethods);
 
 app.listen(3000);
