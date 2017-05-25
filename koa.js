8bf553e1f300b3814734752aefa561e2e0b18f37
arisgi <m1so.tama16@gmail.com>
2017-05-26 01:58:53 +0900

    Fix template configuration

--- koa.js
+++ koa.js
@@ -7,14 +7,14 @@ const app = new Koa();
 const router = Router();
 
 app.use(serve('public'));
-app.use(views('public', { extensions: 'pug' }));
+app.use(views('public', { map: { html: 'pug' } }));
 
 router.get('/', async (ctx, next) => {
-  await ctx.render('index');
+  await ctx.render('index.pug');
 });
 
 router.get('/sp', async (ctx, next) => {
-  await ctx.render('sp');
+  await ctx.render('sp.pug');
 });
 
 app.use(router.routes());
