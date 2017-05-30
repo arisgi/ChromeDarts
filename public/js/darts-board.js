7a95c753fa3595048f2f4dcd7a33e608f8bfb2f0
arisgi <m1so.tama16@gmail.com>
2017-05-31 02:39:19 +0900

    Change background color of darts borad

--- public/js/darts-board.js
+++ public/js/darts-board.js
@@ -8,7 +8,7 @@ function init() {
   const camera = new THREE.PerspectiveCamera(45, window.innerWidth / window.innerHeight, 0.1, 1000);
   const renderer = new THREE.WebGLRenderer();
 
-  renderer.setClearColor(new THREE.Color(0xAAAAAA));
+  renderer.setClearColor(new THREE.Color(0x242E41));
   renderer.setSize(window.innerWidth, window.innerHeight);
 
   let geo;
