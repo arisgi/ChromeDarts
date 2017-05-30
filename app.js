c44a4aa4e8fc32329ca24b1f209d638bda5a5d23
arisgi <m1so.tama16@gmail.com>
2017-05-30 16:29:46 +0900

    Calculate darts position of y axis

--- app.js
+++ app.js
@@ -66,6 +66,30 @@ io.on('connection', (socket) => {
 
   // send the smart phone's sensor data to the browser
   socket.on('throw', (data) => {
+    console.log(data);
+
+    let deg, rad;
+    const correctDeg = 15;
+    const weightVel = 0.5;
+    const correctVel = 5;
+    // culculate deg
+    if (data.orientation.x >= 0) {
+      deg = (180 + correctDeg) - data.orientation.x;
+    } else {
+      if (data.orientation.x <= (correctDeg - 180)) {
+        deg = Math.abs(data.orientation.x - correctDeg) - 180;
+      } else {
+        deg = -(180 - Math.abs(data.orientation.x - correctDeg));
+      }
+    }
+    rad = deg * Math.PI / 180;
+
+    let t = 2.44 / (((data.acceleration.y * weightVel) + correctVel) * Math.cos(rad));
+    let y = (((data.acceleration.y * weightVel) + correctVel) * Math.sin(rad) * t) - (4.9 * t * t);
+
+    console.log(`t: ${t}`);
+    console.log(`y: ${y}`);
+
     io.emit('darts', (data));
   });
 });
